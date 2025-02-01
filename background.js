a49683abfa1284ea2621a0995c76087d82f26024
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2025-02-01 12:32:14 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+// Get Wikidata Entity info 
+const get_entity_info = async (wikiuri) => {
+
+    const res = await fetch(`https://www.wikidata.org/wiki/Special:EntityData/${wikiuri}.json`)
+    const wiki = await res.json()
+    
+    let wikibody = Object.values(wiki.entities)[0]
+    let lastmod = wikibody.modified.split("T")[0] // Last modification log
+    let sitelinks_count = Object.keys(wikibody.sitelinks).length // Number of sitelinks
+    let statements_count = (JSON.stringify(wikibody.claims).match(new RegExp("statement", "g")) || []).length // Number of statements
+    
+    return `${statements_count} statements, ${sitelinks_count} sitelinks - ${lastmod}`
+
+}
+
 // Listen for connection
 chrome.runtime.onConnect.addListener(function(port) {
     
@@ -8,26 +23,38 @@ chrome.runtime.onConnect.addListener(function(port) {
         if (msg.event === "wapi_request") {
 
             // URL creation
-            let url_start = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search="
-            let url_end = "&format=json&language=it&uselang=it&type=item&limit=10&origin=*"
+            let url = "https://www.wikidata.org/w/api.php"
+            url += `?action=wbsearchentities`
+            url += `&search=${msg.text}`
+            url += `&format=json`
+            url += `&language=it`
+            url += `&uselang=it`
+            url += `&type=item`
+            url += `&limit=10`
+            url += `&origin=*`
 
             // Wikidata API Call
-            fetch(url_start + msg.text + url_end)
+            fetch(url)
             .then((response) => response.json())
-            .then(data => {
-                
+            .then(async (data) => {
+
                 try {
-                    // Map response 
-                    const filterList = data['search'].map((ent) => ({
+                    
+                    // Result mapping
+                    const filterList = Promise.all(data['search'].map(async (ent) => ({
                         id: ent.id,
                         label: ent.label,
                         description: ent.description,
-                        uri: ent.concepturi
-                    }))
+                        uri: ent.concepturi,
+                        info: await get_entity_info(ent.id)
+                    }))) 
+                    
+                    // Send array of objects as response to popup.js
+                    port.postMessage({event: "wapi_response", entities: await filterList});                   
 
-                    // Send response to extension
-                    port.postMessage({event: "wapi_response", entities: filterList});
                 } catch {
+
+                    // Send empty array as response to popup.js
                     port.postMessage({event: "wapi_response", entities: []});
                 }
 
